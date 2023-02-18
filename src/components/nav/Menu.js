e931a3154b545506c8526d5c293af4f7ba0038aa
willyongaks <william.ongaki10@gmail.com>
2023-02-18 15:38:02 +0100

    Update Menu.js

--- src/components/nav/Menu.js
+++ src/components/nav/Menu.js
@@ -3,10 +3,8 @@ import { Link, useNavigate } from 'react-router-dom';
 import AuthContext from '../../context/AuthContext';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 
 function Menu() {
