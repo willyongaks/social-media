f4016e2c08e6acfb8c968266deaceb48ccb3a3f5
willyongaks <william.ongaki10@gmail.com>
2023-02-19 14:31:16 +0100

    Update FollowProfile.js

--- src/components/pages/profile/FollowProfile.js
+++ src/components/pages/profile/FollowProfile.js
@@ -1,60 +1,50 @@
-import React, { useState, useEffect, useContext } from 'react';
-import { Base_Post_Url } from '../../../constants/url/BaseUrl';
-import AuthContext from '../../../context/AuthContext';
-
+import React, { useState, useEffect, useContext } from "react";
+import AuthContext from "../../../context/AuthContext";
+import { Base_Post_Url } from "../../../constants/url/BaseUrl";
 
 function FollowProfile({ name, followers }) {
-  const [following, setFollowing] = useState(false);
+	const [following, setFollowing] = useState(false);
 
-  const url = Base_Post_Url + 'profiles';
+	const [auth] = useContext(AuthContext);
 
-  const auth = useContext(AuthContext)
- 
+	const url = Base_Post_Url + "profiles";
 
-  useEffect(() => {
-    const nameInFollowArray = followers.some((follower) => follower.name === auth.name);
-    setFollowing(nameInFollowArray);
-  },[name,followers,setFollowing,auth.name])
+	useEffect(() => {
+		const nameIsInFollowersArray = followers.some((follower) => follower.name === auth.name);
+		setFollowing(nameIsInFollowersArray);
+	}, [followers, name, setFollowing, auth.name]);
 
-  const handleFollow = async () => {
-  try {
-    if (!following) {
-      const follow = await fetch(`${url}/${name}/follow`, {
-        method: 'PUT',
-        headers: {
-          Authorization: `Bearer ${auth.accessToken}`,
-        },
-      });
-      if (!follow.ok) {
-        throw new Error(follow.statusText);
-      }
-      setFollowing(true);
-      localStorage.setItem(`following_${name}`, true);
-    } else {
-      const unFollow = await fetch(`${url}/${name}/unfollow`, {
-        method: 'PUT',
-        headers: {
-          Authorization: `Bearer ${auth.accessToken}`,
-        },
-      });
-      if (!unFollow.ok) {
-        throw new Error(unFollow.statusText);
-      }
-      setFollowing(false);
-      localStorage.removeItem(`following_${name}`);
-    }
-  } catch (error) {
-    console.log(error);
-  }
-};
+	const handleFollow = async () => {
+		try {
+			if (!following) {
+				const follow = await fetch(`${url}/${name}/follow`, {
+					method: "PUT",
+					headers: {
+						Authorization: `Bearer ${auth.accessToken}`,
+					},
+				});
+				if (!follow.ok) {
+					throw new Error(follow.statusText);
+				}
+				setFollowing(true);
+			} else {
+				const unFollow = await fetch(`${url}/${name}/unfollow`, {
+					method: "PUT",
+					headers: {
+						Authorization: `Bearer ${auth.accessToken}`,
+					},
+				});
+				if (!unFollow.ok) {
+					throw new Error(unFollow.statusText);
+				}
+				setFollowing(false);
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
-  return (
-    <>
-      <button onClick={handleFollow}>
-        {following ? 'Unfollow' : 'Follow'}
-      </button>
-    </>
-  );
+	return <button onClick={handleFollow}>{following ? "Unfollow" : "Follow"}</button>;
 }
 
 export default FollowProfile;
