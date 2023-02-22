9f1ad2a138db24bff6a480e4d6abb35b341765bd
willyongaks <william.ongaki10@gmail.com>
2023-02-22 13:59:58 +0100

    n

--- src/components/pages/home/Home.js
+++ src/components/pages/home/Home.js
@@ -4,16 +4,17 @@ import Col from 'react-bootstrap/Col';
 import Posts from '../posts/Posts';
 import Profiles from '../profile/Profiles';
 import CreatePost from '../posts/CreatePost';
+import '../../../App.css';
 
 
 function Home() {
   return (
     <>
       <Row col-12>
-        <Col className='Post-container'>
+        <Col className='Post-container xs={12} md={8}'>
           <Posts />
         </Col>
-        <Col className='col-xl-4 col-md-4 col-12  right-column'>
+        <Col className='col-xl-4 col-md-4 col-12 right-column col d-none d-md-block'>
         <CreatePost />
         <Profiles />
         </Col>
